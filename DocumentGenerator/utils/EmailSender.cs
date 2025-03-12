416a74215db0c9a7d8d2bfbd029fb05682689e49
Soundflog <108269837+Soundflog@users.noreply.github.com>
2025-03-12 16:01:20 +0300

    Create EmailSender
    Класс для отправки email с вложением

---
+++ DocumentGenerator/utils/EmailSender.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Mail;
+using DocumentGenerator.Models;
+
+namespace DocumentGenerator.utils;
+
+// Класс для отправки email с вложением
+public static class EmailSender
+{
+    public static bool SendEmail(string recipient, string subject, string body, byte[] attachmentContent,
+        string fileExtension)
+    {
+        try
+        {
+            var mail = new MailMessage();
+            var smtpServer = new SmtpClient("smtp.example.com");
+            mail.From = new MailAddress("your_email@example.com");
+            mail.To.Add(recipient);
+            mail.Subject = subject;
+            mail.Body = body;
+
+            using var stream = new MemoryStream(attachmentContent);
+            var fileName = "document" + fileExtension;
+            var attachment = new Attachment(stream, fileName);
+            mail.Attachments.Add(attachment);
+
+            smtpServer.Port = 587;
+            smtpServer.Credentials = new NetworkCredential("your_email@example.com", "your_password");
+            smtpServer.EnableSsl = true;
+
+            smtpServer.Send(mail);
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Logger.Log("Ошибка отправки email", ex.Message);
+            return false;
+        }
+    }
+}
