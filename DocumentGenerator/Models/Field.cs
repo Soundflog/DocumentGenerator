3669292a3a08332a0ace6d21c6ee3eca6c04fe0d
Soundflog <108269837+Soundflog@users.noreply.github.com>
2025-03-12 16:00:25 +0300

    Create Models

---
+++ DocumentGenerator/Models/Field.cs
@@ -0,0 +1,16 @@
+﻿namespace DocumentGenerator.Models;
+
+// Класс для представления поля, которое нужно заполнить
+public enum FieldType
+{
+    String,
+    Numeric
+}
+
+public class Field
+{
+    public string Name { get; set; }
+    public string Value { get; set; }
+    public FieldType DataType { get; set; }
+    public string Description { get; set; }
+}
---
+++ DocumentGenerator/Models/LogEntry.cs
@@ -0,0 +1,8 @@
+﻿namespace DocumentGenerator.Models;
+
+public class LogEntry
+{
+    public System.DateTime Timestamp { get; set; }
+    public string OperationType { get; set; }
+    public string Details { get; set; }
+}
